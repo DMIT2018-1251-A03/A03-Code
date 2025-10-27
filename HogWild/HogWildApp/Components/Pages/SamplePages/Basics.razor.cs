e0a21fd4aedbedc1c72adb9be639ad589cc487f8
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-10-27 13:03:50 -0600

    Updated comments

--- HogWild/HogWildApp/Components/Pages/SamplePages/Basics.razor.cs
+++ HogWild/HogWildApp/Components/Pages/SamplePages/Basics.razor.cs
@@ -17,19 +17,33 @@
 
         #region Methods
         // This method is automatically called when the component is initialized
+        // This method should ALWAYS be the first method in your partial class if used
+        // For best organization, put all override methods at the top before other methods
+        // Example: When the page is opened
         protected override void OnInitialized()
         {
             RandomValue();
+            // Calls the base class OnInitialized method (if any)
+            // Note: You do not need to include this unless you have
+            // specifically created a new BaseComponent
+            // The default OnInitialized methods in the default base component
+            // are EMPTY
+            // For our class this is NOT needed
             base.OnInitialized();
         }
 
-       
+
+        /// <summary>
+        /// Generates a random number between 0 and 25 using the Random class
+        /// </summary>
         private void RandomValue()
         {
-            // Create an instance of the Random class to generate random numbers
-            Random rnd = new Random();
+            // Create an instance of the Random class to generate random numbers.
+            Random rnd = new();
 
-            //  Gereate a random integer between 0 (inclusive) and 25 (exclusive)
+            // Generate a random integer between 0 (inclusive) and 25 (exclusive)
+            // Note: Inclusive means that 0 in included as a possibility while
+            // exclusive means that 25 is not a possible value that will be generated
             oddEvenValue = rnd.Next(0, 25);
         }
 
