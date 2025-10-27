d1e3fbef457980f76194627a6d97a58e5515471c
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-10-27 11:42:47 -0600

    Initial Checkin

---
+++ HogWild/HogWildApp/Components/Pages/SamplePages/Basics.razor.cs
@@ -0,0 +1,39 @@
+﻿namespace HogWildApp.Components.Pages.SamplePages
+{
+    public partial class Basics
+    {
+        #region Fields
+        private const string MY_NAME = "James";
+        private int oddEvenValue = 0;
+        private string emailText = string.Empty;
+        private string passwordText = string.Empty;
+        private DateTime dateText = DateTime.Now;
+        #endregion
+
+        #region Properties
+        private bool IsEven => oddEvenValue % 2 == 0;
+
+        #endregion
+
+        #region Methods
+        // This method is automatically called when the component is initialized
+        protected override void OnInitialized()
+        {
+            RandomValue();
+            base.OnInitialized();
+        }
+
+       
+        private void RandomValue()
+        {
+            // Create an instance of the Random class to generate random numbers
+            Random rnd = new Random();
+
+            //  Gereate a random integer between 0 (inclusive) and 25 (exclusive)
+            oddEvenValue = rnd.Next(0, 25);
+        }
+
+        #endregion
+
+    }
+}
