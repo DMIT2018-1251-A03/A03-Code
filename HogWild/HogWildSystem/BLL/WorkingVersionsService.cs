e7db0d843ed46c3d46139ccba68dd911d62bbc45
James Thompson <jthompson@nait.ca>
2025-10-28 09:49:33 -0600

    Oct 28

---
+++ HogWild/HogWildSystem/BLL/WorkingVersionsService.cs
@@ -0,0 +1,38 @@
+﻿
+using HogWildSystem.DAL;
+using HogWildSystem.ViewModels;
+
+namespace HogWildSystem.BLL
+{
+    public class WorkingVersionsService
+    {
+        #region Fields
+        //  hog wild context
+        private readonly HogWildContext _hogWildContext;
+        #endregion
+
+        //  constructor for the WorkingVersionsService class.
+        internal WorkingVersionsService(HogWildContext hogWildContext)
+        {
+            //  Initialize the _hogWildContext field with the provided HogWildContext instance.
+            _hogWildContext = hogWildContext;
+        }
+
+        //  This method retrieves the working version of the resource.
+        public WorkingVersionsView GetWorkingVersion()
+        {
+            return _hogWildContext.WorkingVersions
+                .Select(x => new WorkingVersionsView
+                    {
+                        VersionID = x.VersionId,
+                        Major = x.Major,
+                        Minor = x.Minor,
+                        Build = x.Build,
+                        Revision = x.Revision,
+                        AsOfDate = x.AsOfDate,
+                        Comments = x.Comments
+                    }
+                ).FirstOrDefault() ?? new WorkingVersionsView();
+        }
+    }
+}
